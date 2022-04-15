d3068f802669cf6ef9bfea28892198f4e48bcef7
Alexandra <alexandra.chamero-rossy@hotmail.com>
2022-04-15 10:32:49 +0200

    Exercise business card

---
+++ business_card.py
@@ -0,0 +1,16 @@
+# you could use your first name, last name, 
+# phone number, city, email address…
+
+business_card = {"first name": "Harry",
+                 "last name": "Potter",
+                 "phone number": 78118299, 
+                 "city": "London",
+                 "email adress": "harry.potter@poudlard.com"}
+                
+print(business_card["first name"])
+print(business_card["city"])
+print(business_card["email adress"])
+
+#Once done, use the print() function to only display
+#  your first name and two other 
+# information found in your dictionary.
